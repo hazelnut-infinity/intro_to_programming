bc5ec6e9cc77898cd9b8ac6454dff8218af66613
hazelnut-infinity <apondihazel@gmail.com>
2016-10-03 14:28:19 +0100

    bankers_cheque

--- 092716/fiw.rb
+++ 092716/fiw.rb
@@ -5,7 +5,7 @@
       17 => "seventeen",18 => "eighteen",19 => "nineteen",20 => "twenty", 21 =>"twenty one", 22 => "twenty two", 
       23 => "twenty three", 24 => "twenty four", 25 =>"twenty five", 26 => "twenty six",27 => "twenty seven", 
       28 => "twenty eight", 29 => "twenty nine",30 => "thirty", 31 => "thirty one", 32 => "thirty two", 
-      33 => "thirty three",34 => "thrity four", 35 => "thirty five", 36 => "thirty six", 37 => "thirty seven", 
+      33 => "thirty three",34 => "thirty four", 35 => "thirty five", 36 => "thirty six", 37 => "thirty seven", 
       38 => "thirty eight", 39 => "thirty nine", 40 => "forty",41 => "forty one", 42 => "forty two", 
       43 => "forty three", 44 => "forty four", 45 => "forty five", 46 => "forty six", 47 => "forty seven",
       48 => "forty eight", 49 => "forty nine", 50 => "fifty", 51 => "fifty one", 52 => "fifty two", 53 => "fifty three",
@@ -26,42 +26,66 @@
       1000000000000 => "trillion",
       "&" => "and",
       "$" => "only"
-  }
+        }
 
 puts "Your number here: "
 type = gets.chomp.split("").map(&:to_i) #gets input from the user,converts it into an array and maps it to an integer
 ary = [] #create an empty array
 first2 = type.first(2).join.to_i #define the first two integers
 last2 = type.last(2).join.to_i #define the last two integers
-next2 = [type[1],type[2]].join.to_i #define two consecutive integers
+
+next1 = [type[1],type[2]].join.to_i #define two consecutive integers
+next2 = [type[2],type[3]].join.to_i
+next3 = [type[3],type[4]].join.to_i
+next4 = [type[4],type[5]].join.to_i
+next5 = [type[5],type[6]].join.to_i
+next6 = [type[6], type[7]].join.to_i
+next7 = [type[7],type[8]].join.to_i
+next8 = [type[8],type[9]].join.to_i
+
+
 if type.length == 1
  ary.push([full[type[0]], full["$"]].join(" "))
+
 elsif type.length == 2
-    ary.push([full[first2], full["$"]].join(" "))   
+    ary.push([full[first2], full["$"]].join(" "))
+
 elsif type.length == 3
-    ary.push([full[type[0]],full[100],full["&"],full[last2], full["$"]].join(" "))  #first index, append 100 to it then add and
+    ary.push([full[type[0]],full[100], full["&"], full[last2], full["$"]].join(" "))  #first index, append 100 to it then add and
+
 elsif type.length == 4
-     ary.push([full[type[0]],full[1000],full[type[1]],full[100],full["&"],full[last2], full["$"]].join(" "))
+     ary.push([full[type[0]],full[1000], full[type[1]],full[100],full["&"],full[last2], full["$"]].join(" "))
+
 elsif type.length == 5
-      ary.push([full[first2],full[1000],full[type[1]],full[100],full["&"], full[last2], full["$"]].join(" "))
+      ary.push([full[first2],full[1000],full[type[2]],full[100],full["&"], full[last2], full["$"]].join(" "))
+
 elsif type.length == 6
-      ary.push([full[type[0]],full[100],full["&"],full[next2],full[1000],full[type[3]],full[100],full["&"],full[last2], full["$"]].join(" "))
+      ary.push([full[type[0]],full[100],full["&"],full[next1],full[1000],full[type[3]],full[100],full["&"],full[last2], full["$"]].join(" "))
+
 elsif type.length == 7
-  ary.push([full[type[0]],full[1000000],full[type[1]],full[100],full["&"],full[next2],full[1000],full[type[4]], full[100], full["&"], full[last2], full["$"]].join(" "))     
+  ary.push([full[type[0]],full[1000000],full[type[1]],full[100],full["&"],full[next1],full[1000],full[type[4]], full[100], full["&"], full[last2], full["$"]].join(" "))
+
 elsif type.length == 8
-  ary.push([full[first2], full[1000000],full[type[2]],full[100],full["&"],full[next2],full[1000], full[type[5]],full[100],full["&"],full[last2], full["$"]].join(" "))            
+  ary.push([full[first2], full[1000000],full[type[2]],full[100],full["&"],full[next3],full[1000], full[type[5]],full[100],full["&"],full[last2], full["$"]].join(" "))            
 
 elsif type.length == 9
-   ary.push([full[type[0]],full[1000000000],full[type[1]],full[100],full["&"],full[next2],full[1000000],full[type[4]], full[100], full["&"],full[next2],full[1000],full[type[6]],full[100], full["&"], full[last2], full["$"]].join(" "))
+   ary.push([full[type[0]],full[100], full["&"], full[next1], full [1000000],full[type[3]],full[100],full["&"],full[next4],full[1000],full[type[6]], full[100], full["&"],full[last2], full["$"]].join(" "))
+
 elsif type.length == 10
-      ary.push([full[first2], full[1000000000], full[type[2]], full[100], full["&"], full[next2], full[1000000], full[type[4]], full[100], full[next2], full[1000], full[7], full[100],full["&"], full[last2], full["$"]].join(" "))
+      ary.push([full[type[0]], full[1000000000], full[type[1]], full[100], full["&"], full[next2], full[1000000], full[type[4]], full[100], full["&"], full[next5], full[1000], full[type[7]], full[100],full["&"], full[last2], full["$"]].join(" "))
+
 elsif type.length == 11
-      ary.push([full[type[0]], full[100], full["&"], full[next2], full[1000000000], full[type[3]], full[100], full[next2], full[1000000], full[type[6]], full[100], full["&"], full[next2],full[1000], full[type[9]], full[100], full["&"], full[last2], full["$"]].join(" "))
+      ary.push([full[first2], full[1000000000], full[type[2]], full[100], full["&"], full[next3], full[1000000], full[type[5]], full[100], full["&"], full[next6], full[1000], full[type[8]], full[100], full["&"], full[last2], full["$"]].join(" "))
+
 elsif type.length == 12
-ary.push([full[type[0]], full[1000000000000], full[type[1]], full[100], full["&"], full[next2], full[1000000000], full[type[4]], full[100], full["&"], full[next2], full[1000000], full[type[7]],full[100], full["&"], full[next2], full[1000], full[10], full[100], full["&"], full[last2], full["$"]].join(" "))      
+      ary.push([full[type[0]], full[100], full["&"], full[next1], full[1000000000], full[type[3]], full[100], full["&"], full[next4], full[1000000], full[type[6]], full[100], full["&"], full[next7], full[1000], full[type[9]], full[100], full["&"], full[last2], full["$"]].join(" "))     
+
+elsif type.length == 13
+ary.push([full[type[0]], full[1000000000000], full[type[1]], full[100], full["&"], full[next2], full[1000000000], full[type[4]], full[100], full["&"], full[next5], full[1000000], full[type[7]],full[100], full["&"], full[next8], full[1000], full[type[10]], full[100], full["&"], full[last2], full["$"]].join(" "))      
 end 
 
-puts ary   
+puts ary
+
 
 
 
